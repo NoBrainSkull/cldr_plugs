50e680ad4fd55ecbd5794cd9934c1f47603c06b5
cprice <cprice@tutanota.com>
2025-05-02 11:08:18 +0200

    [debug] - extend logs

--- lib/cldr/plug/plug_put_locale.ex
+++ lib/cldr/plug/plug_put_locale.ex
@@ -230,7 +230,7 @@ defmodule Cldr.Plug.PutLocale do
   @doc false
   def call(conn, options) do
     IO.puts("[DEBUG ON]")
-    IO.inspect(conn.private)
+    IO.inspect(conn.private, label: :before_put_locale)
 
     if locale = locale_from_params(conn, options[:from], options) || default(conn, options) do
       Enum.each(options[:apps], fn app ->
@@ -288,7 +288,6 @@ defmodule Cldr.Plug.PutLocale do
     Enum.reduce_while(from, nil, fn param, _acc ->
       conn
       |> fetch_param(param, options[:param], options)
-      |> IO.inspect(label: inspect(param))
       |> return_if_valid_locale
     end)
   end
@@ -354,7 +353,7 @@ defmodule Cldr.Plug.PutLocale do
   defp fetch_param(conn, :route, _param, options) do
     conn
     |> Map.fetch!(:private)
-    |> IO.inspect()
+    |> IO.inspect(label: :fetch_route)
     |> Map.get(@private_key)
     |> Cldr.validate_locale(options[:cldr])
   end
